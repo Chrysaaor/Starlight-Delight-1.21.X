f212064da6b798148a776d8839b00fd852bea006
Chrysaor <poulpakus.game@gmail.com>
2024-12-02 21:37:46 +0100

    trying to add mixin for custom sweep attack

---
+++ src/main/java/net/chrysaor/chrysaormod/mixin/PlayerEntityMixin.java
@@ -0,0 +1,43 @@
+package net.chrysaor.chrysaormod.mixin;
+
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin extends LivingEntity {
+
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @ModifyExpressionValue()
+
+    @Override
+    public Iterable<ItemStack> getArmorItems() {
+        return null;
+    }
+
+    @Override
+    public ItemStack getEquippedStack(EquipmentSlot slot) {
+        return null;
+    }
+
+    @Override
+    public void equipStack(EquipmentSlot slot, ItemStack stack) {
+
+    }
+
+    @Override
+    public Arm getMainArm() {
+        return null;
+    }
+}
+
