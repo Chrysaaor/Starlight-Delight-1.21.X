0fe89cd4e3a374a18c3446145928a1a22206ba98
Chrysaor <chrysaaor@gmail.com>
2024-12-14 12:25:57 +0100

    pink garnet horse armor loot table added (nether fortress and dungeons) EVERY ITEM IS OBTAINABLE IN SURVIVAL!!

--- src/main/java/net/chrysaor/chrysaormod/util/ModLootTableModifiers.java
+++ src/main/java/net/chrysaor/chrysaormod/util/ModLootTableModifiers.java
@@ -13,6 +13,11 @@ import net.minecraft.util.Identifier;
 public class ModLootTableModifiers {
     private static final Identifier END_CITY_ID =
             Identifier.ofVanilla("chests/end_city_treasure");
+    private static final Identifier NETHER_BRIDGE_ID =
+            Identifier.ofVanilla("chests/nether_bridge");
+    private static final Identifier SIMPLE_DUNGEON_ID =
+            Identifier.ofVanilla("chests/simple_dungeon");
+
     private static final Identifier SHORT_GRASS_ID =
             Identifier.ofVanilla("blocks/short_grass");
     private static final Identifier TALL_GRASS_ID =
@@ -21,6 +26,8 @@ public class ModLootTableModifiers {
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+
+            //Chests
             if (source.isBuiltin() && END_CITY_ID.equals(key.getValue())) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0f))
@@ -30,6 +37,26 @@ public class ModLootTableModifiers {
 
                 tableBuilder.pool(poolBuilder);
             }
+            if (source.isBuiltin() && NETHER_BRIDGE_ID.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1.0f))
+                        .conditionally(RandomChanceLootCondition.builder(0.2f))
+                        .with(ItemEntry.builder(ModItems.PINK_GARNET_HORSE_ARMOR))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder);
+            }
+            if (source.isBuiltin() && SIMPLE_DUNGEON_ID.equals(key.getValue())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1.0f))
+                        .conditionally(RandomChanceLootCondition.builder(0.3f))
+                        .with(ItemEntry.builder(ModItems.PINK_GARNET_HORSE_ARMOR))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder);
+            }
+
+            //Blocks
             if (source.isBuiltin() && TALL_GRASS_ID.equals(key.getValue())) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1.0f))
