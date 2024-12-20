ccb3beb0d92e4a3fb176866227534ecfdb2ef562
Chrysaor <chrysaaor@gmail.com>
2024-12-20 22:31:14 +0100

    cauliflower buff

--- src/main/java/net/chrysaor/chrysaormod/item/ModFoodComponents.java
+++ src/main/java/net/chrysaor/chrysaormod/item/ModFoodComponents.java
@@ -5,6 +5,6 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 
 public class ModFoodComponents {
-    public static final FoodComponent CAULIFLOWER = new FoodComponent.Builder().nutrition(3).saturationModifier(0.25f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 200), 0.15f).build();
+    public static final FoodComponent CAULIFLOWER = new FoodComponent.Builder().nutrition(4).saturationModifier(0.7f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 400), 0.15f).build();
 }
