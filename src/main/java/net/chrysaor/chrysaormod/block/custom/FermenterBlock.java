76eebae39481d90af38bdeb65a10165af185f232
Chrysaor <chrysaaor@gmail.com>
2025-04-26 01:24:18 +0200

    removed unused imports

--- src/main/java/net/chrysaor/chrysaormod/block/custom/FermenterBlock.java
+++ src/main/java/net/chrysaor/chrysaormod/block/custom/FermenterBlock.java
@@ -13,14 +13,11 @@ import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
